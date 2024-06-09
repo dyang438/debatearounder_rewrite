cffb196a333a43b04282f7e4d4e01b462dca5068
dyang438 <152822883+dyang438@users.noreply.github.com>
2024-06-09 00:09:53 -0700

    finished with thing

--- frontend/src/root.jsx
+++ frontend/src/root.jsx
@@ -4,61 +4,6 @@ import { useNavigate } from "react-router-dom";
 import PersonalFormRoom from "./Form/PersonalFormRoom.jsx";
 import RoomParent from "./Room/RoomParent.jsx";
 import './css/app.css';
-// function Root() {
-//   const { signedIn } = useAuth();
-//   const navigate = useNavigate();
-//
-//   const returnHome = () => {
-//     navigate('/');
-//   }
-//
-//   const postCreation = () => {
-//     navigate('/addPost');
-//   }
-//
-//   const signup = () => {
-//     navigate('/signup');
-//   }
-//
-//
-//   return (
-//     <>
-//       <nav>
-//         <Button onClick={returnHome} style={
-//           {position: 'fixed',
-//           top: '10px',
-//           left: '10px',
-//           fontSize: '20px',
-//           backgroundColor: 'green',}
-//         } variant="contained"> Home </Button>
-//
-//         {signedIn && <Button variant="contained" onClick={postCreation} style={{
-//           position: 'fixed',
-//           top: '10px',
-//           right: '10px',
-//           backgroundColor: 'green',
-//           fontSize: '20px',
-//         }}> Create Post</Button>}
-//         {!signedIn && <Button variant="contained" onClick={signup} style={
-//           {
-//             position: 'fixed',
-//             top: '10px',
-//             right: '10px',
-//             fontSize: '20px',
-//             backgroundColor: 'green',
-//           }
-//         }> Create an Account to Post </Button>}
-//         < LogoutButton />
-//       </nav>
-//
-//       <Routes>
-//         <Route path="/" element={<PostListPage />} />
-//         <Route path="/post/:postId" element={<PostDetailPage />} />
-//         <Route path="/addPost" element={<NewPostPage />} />
-//       </Routes>
-//     </>
-//   );
-// }
 
 function Root() {
   const navigate = useNavigate();
@@ -96,7 +41,7 @@ function Root() {
       <>
           <div id={'landingPage'}>
             <div>
-              <h1> Welcome to DebateArounder! </h1>
+              <h1> Welcome to DebateARounder! </h1>
             </div>
             <Button id={'roomButton'} onClick={createRoom}> Create a Room </Button>
             <Button id={'formButton'} onClick={goToForm}> Go to Form </Button>
